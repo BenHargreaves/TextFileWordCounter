f3879e78b6cc7c3105ed2f562e532d80340b47db
BenHargreaves <bhargreaves91@gmail.com>
2017-03-03 22:26:07 -0500

    Add project files.

---
+++ TextFileWordCounter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileWordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = File.OpenText("File.txt"); 
+            //string enteredText;
+            int countedText;
+            char[] stringSpliter = new char[] {' ','\n','\r'};
+
+            Console.WriteLine("Welcome to the Word Counter Program!");
+            //Console.WriteLine("\nPlease Enter some Text below:");
+            //enteredText = Console.ReadLine();
+            string fileString = reader.ReadToEnd();
+
+
+            countedText = fileString.Split(stringSpliter, StringSplitOptions.RemoveEmptyEntries).Length;
+
+            Console.WriteLine(countedText);
+            //Console.WriteLine(fileString);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
